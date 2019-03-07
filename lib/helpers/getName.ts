a1e1a7edb643940231e10587c7c3371078d912d5
Vladislav Kobyakov <ayakudere@gmail.com>
2019-03-07 16:58:59 +0300

    fix: add transliterating for minio filenames

--- lib/helpers/getName.ts
+++ lib/helpers/getName.ts
@@ -1,9 +1,14 @@
+import CyrillicToTranslit from 'cyrillic-to-translit-js'
 import * as md5 from 'md5'
 
 import { getParts } from './getParts'
 
 export const getName = (data: Buffer, originalName: string): string => {
-  const { baseName, extension } = getParts(originalName)
+  const transliterator = new CyrillicToTranslit()
+
+  const transliteratedName = transliterator.transform(originalName)
+
+  const { baseName, extension } = getParts(transliteratedName)
 
   return `${md5(data)}_${baseName}.${extension}`
 }
